2000a2e7a8fe3a478caa224fe775b89655e7029a
Dielz <darkfalz674@gmail.com>
2023-11-06 02:30:25 -0400

    .

--- src/preload.ts
+++ src/preload.ts
@@ -1,7 +1,8 @@
 import { OpenAI } from "openai";
 //import { TextToSpeechClient } from "@google-cloud/text-to-speech";
 //import { google } from "@google-cloud/text-to-speech/build/protos/protos";
-import { ResultReason, SpeechConfig, SpeechSynthesizer } from "microsoft-cognitiveservices-speech-sdk";
+import * as WavEncoder from "wav-encoder";
+import { AudioConfig, ResultReason, SpeechConfig, SpeechRecognizer, SpeechSynthesizer } from "microsoft-cognitiveservices-speech-sdk";
 import * as path from "path";
 //import * as fs from "fs";
 //import * as util from "util";
@@ -40,9 +41,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
     navigator.mediaDevices.getUserMedia(constraints)
       .then(function (stream) {
-        const audioContext = new AudioContext();
+        //const audioContext = new AudioContext();
         audioStream = stream;
-        const input = audioContext.createMediaStreamSource(stream);
+        //const input = audioContext.createMediaStreamSource(stream);
         rec = new MediaRecorder(stream);
         rec.start();
 
@@ -82,20 +83,74 @@ window.addEventListener("DOMContentLoaded", () => {
     transcript(blob);
   }
 
+
+
   async function transcript(blob: Blob) {
-    const transcript = await openaiApi.audio.transcriptions.create({
+
+    //   // Obtén los datos de audio del Blob
+    //   let arrayBuffer = await blob.arrayBuffer();
+    //   // Asegurémonos de que la longitud sea múltiplo de 4
+    //   const dataView = new DataView(arrayBuffer);
+    //   const originalLength = dataView.byteLength;
+    //   const padding = 4 - (originalLength % 4);
+    //   if (padding !== 4) {
+    //     const paddedArrayBuffer = new ArrayBuffer(originalLength + padding);
+    //     const paddedDataView = new DataView(paddedArrayBuffer);
+    //     for (let i = 0; i < originalLength; i++) {
+    //       paddedDataView.setUint8(i, dataView.getUint8(i));
+    //     }
+    //     arrayBuffer = paddedArrayBuffer;
+    //   }
+
+    //   const audioData: WavEncoder.AudioData = {
+    //     sampleRate: 44100,
+    //     channelData: [new Float32Array(arrayBuffer)],
+    //   };
+
+    //   // Agrega un encabezado WAV
+    //   let wavData = null;
+    //   await WavEncoder.encode(audioData).then((buffer) => {
+    //     wavData = buffer;
+    //   });
+
+    //   const wavBlob = new Blob([wavData], { type: 'audio/wav' });
+
+    //   // Microsoft ---------------------------------------------------------------------------------------------------------------------
+
+
+    //   let audioConfig = AudioConfig.fromWavFileInput(new File([wavBlob], 'audio.wav'));
+    //   let speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+    // //  play2(blob);
+    //   let transcript = "Hola";
+
+    //   await speechRecognizer.recognizeOnceAsync((result) => {
+    //     speechRecognizer.close();
+    //     console.log(result);
+    //     transcript = result.text;
+    //     console.log(transcript);
+
+    //   }, (err) => {
+
+    //     console.log(err);
+    //   });
+
+    //   document.getElementById("question").innerHTML = transcript;
+
+    // OpenAi ------------------------------------------------------------------------------------------------------------------------
+    const transcriptG = await openaiApi.audio.transcriptions.create({
       model: 'whisper-1',
       file: new File([blob], 'audio.wav'),
     });
 
-    document.getElementById("question").innerHTML = transcript.text;
+    let transcript = transcriptG.text;
+    document.getElementById("question").innerHTML = transcript;
 
+
+    // OpenAi ------------------------------------------------------------------------------------------------------------------------
     const completion = await openaiApi.completions.create({
       model: 'gpt-3.5-turbo-instruct',
-      prompt: `Que tu respuesta sea breve y corta y nada de código o caracteres ilegibles y
-     que todas tus respuestas estan orientada hacia la navidad y en caso de no poder orientarla a la navidad o 
-     un tema relacionado a la navidad, responde con "No puedo responder nada fuera de la navidad, pero si te 
-     puedo dar un dato navideño" y procedes a dar un dato sobre la navidad o un chiste navideño muy gracioso y utiliza gerga dominicana. "${transcript.text}"`,
+      prompt: `Que tu respuesta sea breve y corta y evita usar códigos o caracteres ilegibles o iconos, que todas tus respuestas estén orientadas a la navidad y en caso de no poder orientarla a la navidad o un tema relacionado a la navidad, responde con "No puedo responder temas no relacionados con la navidad, pero sí te puedo dar un dato navideño" y procedes a dar un dato sobre la navidad.  "${transcript}"`,
       temperature: 0.7,
       max_tokens: 256,
       top_p: 1,
@@ -151,6 +206,12 @@ window.addEventListener("DOMContentLoaded", () => {
   function play(audioContent: Uint8Array) {
     const blob = new Blob([audioContent], { type: 'audio/mp3' });
     const audioUrl = URL.createObjectURL(blob);
+    const audio = new Audio(audioUrl);
+    audio.play();
+  }
+
+  function play2(audioContent: Blob) {
+    const audioUrl = URL.createObjectURL(audioContent);
     const audio = new Audio(audioUrl);
     audio.play();
   }
